a784eb255bc92091519de23061ab2844378cff62
Mohammad Shams <m.shamsahmar@gmail.com>
2022-12-15 18:12:50 -0500

    generated CW and CCW tilts of the source images

--- stimulus/gen_tilted_images.py
+++ stimulus/gen_tilted_images.py
@@ -9,18 +9,27 @@ from PIL import Image
 import numpy as np
 import os
 
+# face or house
+cat = 'house'
+
 # set the source image path
-source_path = os.path.join("images", "source", "face.png")
+source_path = os.path.join("images", "source", f"{cat}.png")
 # set the save image path
 save_path = os.path.join("images")
 # read the image
 im = Image.open(source_path)
 # define the span of tilts (degrees)
-ch_min = 0
-ch_max = 3
-ch_step = 0.1
-factors = np.arange(ch_min, ch_max, ch_step)
-
+min_tilt = 0
+max_tilt = 3
+step_tilt = 0.1
+factors = np.arange(min_tilt, max_tilt, step_tilt)
 for count, factor in enumerate(factors):
     im_output = im.rotate(factor)
-    im_output.save(os.path.join(save_path, f'face_tilt{count}.png'))
+    im_output.save(os.path.join(save_path, f'{cat}_tilt{count}_CCW.png'))
+min_tilt = 0
+max_tilt = -3
+step_tilt = -0.1
+factors = np.arange(min_tilt, max_tilt, step_tilt)
+for count, factor in enumerate(factors):
+    im_output = im.rotate(factor)
+    im_output.save(os.path.join(save_path, f'{cat}_tilt{count}_CW.png'))
