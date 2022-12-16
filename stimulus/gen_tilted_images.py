a15aac8116c803e0d1d14f7c4fa679aa10ab09c1
MoShams1 <m.shamsahmar@gmail.com>
2022-12-16 12:19:58 -0500

    increased number of available tilts to plus/minus 5 deg

--- stimulus/gen_tilted_images.py
+++ stimulus/gen_tilted_images.py
@@ -10,26 +10,27 @@ import numpy as np
 import os
 
 # face or house
-cat = 'face'
+cats = ['face', 'house']
 
-# set the source image path
-source_path = os.path.join("images", "source", f"{cat}.png")
-# set the save image path
-save_path = os.path.join("images")
-# read the image
-im = Image.open(source_path)
-# define the span of tilts (degrees)
-min_tilt = 0
-max_tilt = 3
-step_tilt = 0.1
-factors = np.arange(min_tilt, max_tilt, step_tilt)
-for count, factor in enumerate(factors):
-    im_output = im.rotate(factor)
-    im_output.save(os.path.join(save_path, f'{cat}_tilt{count}_CCW.png'))
-min_tilt = 0
-max_tilt = -3
-step_tilt = -0.1
-factors = np.arange(min_tilt, max_tilt, step_tilt)
-for count, factor in enumerate(factors):
-    im_output = im.rotate(factor)
-    im_output.save(os.path.join(save_path, f'{cat}_tilt{count}_CW.png'))
+for cat in cats:
+    # set the source image path
+    source_path = os.path.join("images", "source", f"{cat}.png")
+    # set the save image path
+    save_path = os.path.join("images")
+    # read the image
+    im = Image.open(source_path)
+    # define the span of tilts (degrees)
+    min_tilt = 0
+    max_tilt = 5
+    step_tilt = 0.1
+    factors = np.arange(min_tilt, max_tilt, step_tilt)
+    for count, factor in enumerate(factors):
+        im_output = im.rotate(factor)
+        im_output.save(os.path.join(save_path, f'{cat}_tilt{count}_CCW.png'))
+    min_tilt = 0
+    max_tilt = -5
+    step_tilt = -0.1
+    factors = np.arange(min_tilt, max_tilt, step_tilt)
+    for count, factor in enumerate(factors):
+        im_output = im.rotate(factor)
+        im_output.save(os.path.join(save_path, f'{cat}_tilt{count}_CW.png'))
