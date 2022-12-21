232771364a668bf197a7d889a4350fe406aa3004
york <m.shamsahmar@gmail.com>
2022-12-21 04:26:07 -0500

    to test the multiple event generator

---
+++ stimulus/test_gen_dist_events.py
@@ -0,0 +1,21 @@
+import gen_distributed_events as gen_evnts
+import matplotlib.pyplot as plt
+import numpy as np
+
+REF_RATE = 60
+TRIAL_DUR = 10 * REF_RATE
+len_arr = np.empty((1000,))
+times = []
+for i in range(1000):
+    change_start_frames = gen_evnts.gen_ditributed_event(0,
+                                                         TRIAL_DUR,
+                                                         REF_RATE,
+                                                         TRIAL_DUR - REF_RATE,
+                                                         REF_RATE * 2)
+    len_arr[i] = len(change_start_frames)
+    times = np.hstack((times, change_start_frames))
+
+fig, ax = plt.subplots(2, 1)
+ax[0].hist(len_arr)
+ax[1] = plt.hist(times)
+plt.show()
