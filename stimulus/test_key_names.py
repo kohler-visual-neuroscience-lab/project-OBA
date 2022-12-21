13b33b9299c8fcbe2d484e4388fad97f943222c5
MoShams1 <m.shamsahmar@gmail.com>
2022-12-21 16:40:43 -0500

    to test the name of the keyboard keys

---
+++ stimulus/test_key_names.py
@@ -0,0 +1,15 @@
+from psychopy import event, visual, core
+import supplements as sup
+import pandas as pd
+import numpy as np
+
+# configure the monitor and the stimulus window
+mon = sup.config_mon_dell()
+win = sup.config_win(mon=mon, fullscr=False, screen=0)
+
+key_array = []
+for iframe in range(100):
+    win.flip()
+    key_response = event.waitKeys()
+    print(key_response)
+
