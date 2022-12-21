6e0e70b3f25182664af06f3af1557d5c8dcd9b1a
MoShams1 <m.shamsahmar@gmail.com>
2022-12-21 16:40:14 -0500

    minor change

--- stimulus/test_multiple_events.py
+++ stimulus/test_multiple_events.py
@@ -3,25 +3,25 @@ import supplements as sup
 import pandas as pd
 import numpy as np
 
-# # configure the monitor and the stimulus window
-# mon = sup.config_mon_imac24()
-# win = sup.config_win(mon=mon, fullscr=False, screen=0)
-#
-# key_array = []
-# for iframe in range(120):
-#     key_response = event.getKeys(keyList=['space', 'escape'])
-#     if 'space' in key_response:
-#         print(key_response)
-#         key_array.append(key_response)
-#         event.clearEvents()
-# win.flip()
-# print(key_array)
+# configure the monitor and the stimulus window
+mon = sup.config_mon_dell()
+win = sup.config_win(mon=mon, fullscr=False, screen=0)
 
-timer = core.Clock()
-timer.reset()
-a = np.empty((3,))
-a[:] = np.nan
-print(a)
-for i in range(3):
-    a[i] = timer.getTime()
-print(a)
+key_array = []
+for iframe in range(600):
+    key_response = event.getKeys(keyList=['space', 'escape'])
+    if 'space' in key_response:
+        print(key_response)
+        key_array.append(key_response)
+        event.clearEvents()
+win.flip()
+print(key_array)
+
+# timer = core.Clock()
+# timer.reset()
+# a = np.empty((3,))
+# a[:] = np.nan
+# print(a)
+# for i in range(3):
+#     a[i] = timer.getTime()
+# print(a)
