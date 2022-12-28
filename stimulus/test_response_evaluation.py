27f3517d7f4de434bdf7ea656e3c85342fda85a2
Mohammad Shams <m.shamsahmar@gmail.com>
2022-12-27 19:32:08 -0500

    successfully tested the evaluation function with 14 scenarios

--- stimulus/test_response_evaluation.py
+++ stimulus/test_response_evaluation.py
@@ -1,11 +1,163 @@
 import numpy as np
-from lib import evaluate_response2
+from lib import evaluate_response
 
+n_tests = 14
+perf_arr = []
+expected_perf = np.full(n_tests, np.nan, dtype=int)
+
+# TEST #1
 cue_image = 1
-change_image = np.array([1, 2, 2, 2])
-change_times = np.array([200, 400, 500, 700])
-response_times = np.array([320.34])
+change_image = np.array([2, 1, 2])
+change_times = np.array([2000, 5000, 7000])
+response_times = np.array([])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[0] = 67
 
-[resp_eval, rt] = evaluate_response2(cue_image, change_image,
-                                     change_times, response_times)
-print((resp_eval, rt))
+# TEST #2
+cue_image = 1
+change_image = np.array([2])
+change_times = np.array([2000])
+response_times = np.array([])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[1] = 100
+
+# TEST #3
+cue_image = 2
+change_image = np.array([2])
+change_times = np.array([2000])
+response_times = np.array([])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[2] = 0
+
+# TEST #4
+cue_image = 1
+change_image = np.array([1])
+change_times = np.array([2000])
+response_times = np.array([2500, 4000])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[3] = 0
+
+# TEST #5
+cue_image = 1
+change_image = np.array([1, 1])
+change_times = np.array([1000, 3000])
+response_times = np.array([2500])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[4] = 0
+
+# TEST #6
+cue_image = 1
+change_image = np.array([1, 1])
+change_times = np.array([1000, 3000])
+response_times = np.array([2500, 3200])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[5] = 50
+
+# TEST #7
+cue_image = 1
+change_image = np.array([1, 1, 1])
+change_times = np.array([1000, 3000, 5000])
+response_times = np.array([1500, 2500, 5200, 5300])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[6] = 33
+
+# TEST #8
+cue_image = 1
+change_image = np.array([1, 1, 1])
+change_times = np.array([1000, 3000, 5000])
+response_times = np.array([1500, 4200, 5200])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[7] = 67
+
+# TEST #9
+cue_image = 1
+change_image = np.array([2, 2, 1])
+change_times = np.array([3000, 5000, 7000])
+response_times = np.array([2500, 5200, 5300])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[8] = 0
+
+# TEST #10
+cue_image = 2
+change_image = np.array([1, 2, 2, 2])
+change_times = np.array([1000, 3000, 5000, 7000])
+response_times = np.array([3400, 6100, 7600])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[9] = 75
+
+
+# TEST #11
+cue_image = 2
+change_image = np.array([2, 2, 1])
+change_times = np.array([1000, 2500, 7000])
+response_times = np.array([1500, 7400, 7500])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[10] = 33
+
+
+# TEST #12
+cue_image = 1
+change_image = np.array([1, 2, 1, 2])
+change_times = np.array([1000, 3000, 5000, 7000])
+response_times = np.array([])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[11] = 50
+
+
+# TEST #13
+cue_image = 1
+change_image = np.array([1, 2, 1, 2])
+change_times = np.array([1000, 3000, 5000, 7000])
+response_times = np.array([2100, 3000, 5500])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[12] = 50
+
+
+# TEST #14
+cue_image = 1
+change_image = np.array([1, 1, 1, 1])
+change_times = np.array([1000, 3000, 5000, 7000])
+response_times = np.array([2100, 3000, 5500])
+perf, _ = evaluate_response(cue_image, change_image,
+                            change_times, response_times)
+perf_arr.append(perf)
+expected_perf[13] = 25
+
+
+perf_arr = np.array(perf_arr)
+comp_arr = expected_perf == perf_arr
+test = np.all(comp_arr)
+failed_tests = np.nonzero(comp_arr == 0)[0] + 1
+if test:
+    print("*** TEST PASSED! ***")
+else:
+    print("### MISMATCH FOUND ON TEST(S):")
+    print(failed_tests)
+    print(f"expected perf =  {expected_perf}")
+    print(f"evaluated perf = {perf_arr}")
