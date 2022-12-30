54d1ed49109b475d13f7587896d764ca0bff3469
Mohammad Shams <m.shamsahmar@gmail.com>
2022-12-29 21:30:27 -0500

    updated visualization function to match adobe illustrator's restrictions

--- analysis/cleanplot.py
+++ analysis/cleanplot.py
@@ -1,8 +1,12 @@
+import matplotlib as plt
 
-def trim_axes(ax, xlim, ylim):
+
+def trim_axes(ax):
     # remove the upper and the right borders of the axis
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
-    # limit the axes
-    ax.set_ylim(ymin=ylim[0], ymax=ylim[1])
-    ax.set_xlim(xmin=xlim[0], xmax=xlim[1])
+
+
+def prep4ai():
+    plt.rcParams['pdf.fonttype'] = 42  # AI can detect text now
+    plt.rcParams['font.sans-serif'] = "Arial"
