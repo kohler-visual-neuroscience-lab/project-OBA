6aac90411e4a2fc7d00972c07a50abf3fc0b11b1
Mohammad Shams <m.shamsahmar@gmail.com>
2023-01-10 14:26:08 -0500

    renamed a few variables for better readability

--- stimulus/gen_tilted_images.py
+++ stimulus/gen_tilted_images.py
@@ -1,20 +1,21 @@
 """
 The aim here is to create several versions of an input image with different
 orientations
-Mohammad Shams <m.shams.ahmar@gmail.com>
-last modification: 2022-12-07
+
+Mo Shams <MShamsCBR@gmail.com>
+last modification: 2023-01-10
 """
 
-from PIL import Image
-import numpy as np
 import os
+import numpy as np
+from PIL import Image
 
 # face or house
 cats = ['face', 'house']
 
 for cat in cats:
     # set the source image path
-    source_path = os.path.join("image", "source", f"{cat}.png")
+    source_path = os.path.join("image", "source", f"{cat}_cropped.png")
     # set the save image path
     save_path = os.path.join("image")
     # read the image
@@ -23,22 +24,23 @@ for cat in cats:
     min_tilt = 0
     max_tilt = 5
     step_tilt = 0.1
-    factors = np.arange(min_tilt, max_tilt, step_tilt)
-    for count, factor in enumerate(factors):
-        im_output = im.rotate(factor)
-        if count == 0:
-            im_output.save(os.path.join(save_path, f'{cat}_tilt{count}.png'))
+    mags = np.arange(min_tilt, max_tilt, step_tilt)
+    for imag, mag in enumerate(mags):
+        im_output = im.rotate(mag)
+        if imag == 0:
+            im_output.save(os.path.join(save_path,
+                                        f'{cat}_tilt{imag}.png'))
         else:
-            im_output.save(os.path.join(save_path, f'{cat}_tilt'
-                                                   f'{count}_CCW.png'))
+            im_output.save(os.path.join(save_path,
+                                        f'{cat}_tilt{imag}_CCW.png'))
     min_tilt = 0
     max_tilt = -5
     step_tilt = -0.1
-    factors = np.arange(min_tilt, max_tilt, step_tilt)
-    for count, factor in enumerate(factors):
-        im_output = im.rotate(factor)
-        if count == 0:
-            im_output.save(os.path.join(save_path, f'{cat}_tilt{count}.png'))
+    mags = np.arange(min_tilt, max_tilt, step_tilt)
+    for imag, mag in enumerate(mags):
+        im_output = im.rotate(mag)
+        if imag == 0:
+            im_output.save(os.path.join(save_path, f'{cat}_tilt{imag}.png'))
         else:
             im_output.save(os.path.join(save_path, f'{cat}_tilt'
-                                                   f'{count}_CW.png'))
+                                                   f'{imag}_CW.png'))
