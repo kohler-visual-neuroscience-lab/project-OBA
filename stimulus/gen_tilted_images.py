3f525bd5ccb69059972c35bc93f7c0739227cba6
Mohammad Shams <m.shamsahmar@gmail.com>
2023-01-17 19:41:15 -0500

    modified to include exemplars of an image

--- stimulus/gen_tilted_images.py
+++ stimulus/gen_tilted_images.py
@@ -10,37 +10,43 @@ import os
 import numpy as np
 from PIL import Image
 
-# face or house
+# set the save image path
+save_path = os.path.join("image")
+
+# face or house categories
 cats = ['face', 'house']
+# examples
+exms = list(range(1, 9, 1))
 
 for cat in cats:
-    # set the source image path
-    source_path = os.path.join("image", "source", f"{cat}_cropped.png")
-    # set the save image path
-    save_path = os.path.join("image")
-    # read the image
-    im = Image.open(source_path)
-    # define the span of tilts (degrees)
-    min_tilt = 0
-    max_tilt = 5
-    step_tilt = 0.1
-    mags = np.arange(min_tilt, max_tilt, step_tilt)
-    for imag, mag in enumerate(mags):
-        im_output = im.rotate(mag)
-        if imag == 0:
-            im_output.save(os.path.join(save_path,
-                                        f'{cat}_tilt{imag}.png'))
-        else:
-            im_output.save(os.path.join(save_path,
-                                        f'{cat}_tilt{imag}_CCW.png'))
-    min_tilt = 0
-    max_tilt = -5
-    step_tilt = -0.1
-    mags = np.arange(min_tilt, max_tilt, step_tilt)
-    for imag, mag in enumerate(mags):
-        im_output = im.rotate(mag)
-        if imag == 0:
-            im_output.save(os.path.join(save_path, f'{cat}_tilt{imag}.png'))
-        else:
-            im_output.save(os.path.join(save_path, f'{cat}_tilt'
-                                                   f'{imag}_CW.png'))
+    for exm in exms:
+        # set the source image path
+        source_path = os.path.join("image", "source",
+                                   f"{cat}{exm}_cropped.png")
+        # read the image
+        im = Image.open(source_path)
+        # define the span of tilts (degrees)
+        min_tilt = 0
+        max_tilt = 5
+        step_tilt = 0.1
+        mags = np.arange(min_tilt, max_tilt, step_tilt)
+        for imag, mag in enumerate(mags):
+            im_output = im.rotate(mag)
+            if imag == 0:
+                im_output.save(os.path.join(save_path,
+                                            f'{cat}{exm}_tilt{imag}.png'))
+            else:
+                im_output.save(os.path.join(save_path,
+                                            f'{cat}{exm}_tilt{imag}_CCW.png'))
+        min_tilt = 0
+        max_tilt = -5
+        step_tilt = -0.1
+        mags = np.arange(min_tilt, max_tilt, step_tilt)
+        for imag, mag in enumerate(mags):
+            im_output = im.rotate(mag)
+            if imag == 0:
+                im_output.save(os.path.join(save_path, f'{cat}{exm}_tilt'
+                                                       f'{imag}.png'))
+            else:
+                im_output.save(os.path.join(save_path, f'{cat}{exm}_tilt'
+                                                       f'{imag}_CW.png'))
