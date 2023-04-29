f8435f49959e979f3f51bcffd8c215071b98b702
Mohammad Shams <m.shamsahmar@gmail.com>
2023-04-29 01:17:01 -0400

    analyzed FBA data from April 28th

--- analysis/x008_inspect_session_exp01_v03.py
+++ analysis/x008_inspect_session_exp01_v03.py
@@ -49,8 +49,20 @@ def snr_spectrum(psd, noise_n_neighbor_freqs=3, noise_skip_neighbor_freqs=1):
 
 # (N=1)
 
-eeg_file = '0004_20230403_013324_exp01_v03.mff'
-beh_file = '0004_20230403_123323_exp01_v03.json'
+# eeg_file = '0004_20230403_013324_exp01_v03.mff'
+# beh_file = '0004_20230403_123323_exp01_v03.json'
+
+# eeg_file = '0012_20230414_011358_exp01_v03.mff'
+# beh_file = '0012_20230414_121357_exp01_v03.json'
+
+# eeg_file = '0013_20230414_115805_exp01_v03.mff'
+# beh_file = '0013_20230414_105803_exp01_v03.json'
+
+eeg_file = '1191_20230428_023515_exp01_v03.mff'
+beh_file = '1191_20230428_133513_exp01_v03.json'
+
+# eeg_file = '2002_20230428_014730_exp01_v03.mff'
+# beh_file = '2002_20230428_124727_exp01_v03.json'
 
 
 # set the full path to the raw data
@@ -72,10 +84,14 @@ rec_date = beh_file[5:13]
 
 # /// READ AND CONFIGURE BEHAVIORAL DATA ///
 
-# set number of blocks
-n_blocks = 4
 # extract number of trials
 n_trials = beh_data.shape[0]
+
+# set number of blocks
+# n_blocks = 4
+n_blocks = n_trials / 32  # from session 4 onward, we recorded 2 blocks of
+# 32 trials
+
 # convert tilt magnitudes to degrees
 tilt_angle = (beh_data['tilt_magnitude'].values + 1) / 10
 # ----------------------------------------------------------------------------
@@ -87,7 +103,7 @@ eeg.info['line_freq'] = 60.
 montage = mne.channels.make_standard_montage('GSN-HydroCel-129')
 eeg.set_montage(montage, match_alias=True)
 easycap_montage = mne.channels.make_standard_montage('GSN-HydroCel-129')
-easycap_montage.plot()
+# easycap_montage.plot()
 # set common average reference
 eeg.set_eeg_reference('average', projection=False, verbose=False)
 # apply bandpass filter
@@ -178,19 +194,19 @@ snrs_cnd4_1f2_avg = snrs_cnd4_1f2.mean(axis=0)
 # obrtain "image boost" topography map
 red135_boost_1f1 = snrs_cnd1_1f1_avg - snrs_cnd3_1f1_avg
 red135_boost_1f2 = snrs_cnd2_1f2_avg - snrs_cnd4_1f2_avg
-green45_boost_1f1 = snrs_cnd4_1f1_avg - snrs_cnd2_1f1_avg
-green45_boost_1f2 = snrs_cnd3_1f2_avg - snrs_cnd1_1f2_avg
+blue45_boost_1f1 = snrs_cnd4_1f1_avg - snrs_cnd2_1f1_avg
+blue45_boost_1f2 = snrs_cnd3_1f2_avg - snrs_cnd1_1f2_avg
 
 # average across all channels
 red135_boost = np.mean((red135_boost_1f1 + red135_boost_1f2) / 2)
-green45_boost = np.mean((green45_boost_1f1 + green45_boost_1f2) / 2)
+blue45_boost = np.mean((blue45_boost_1f1 + blue45_boost_1f2) / 2)
 
 # average across occipital channels
 occ_ch = ['E66', 'E69', 'E70', 'E71', 'E73', 'E74', 'E75',
           'E76', 'E81', 'E82', 'E83', 'E84', 'E88', 'E89']
 ind_occ = np.nonzero(np.isin(epochs.info.ch_names, occ_ch))
 red135_boost_occ = np.mean(((red135_boost_1f1 + red135_boost_1f2) / 2)[ind_occ])
-green45_boost_occ = np.mean(((green45_boost_1f1 + green45_boost_1f2) / 2)[ind_occ])
+blue45_boost_occ = np.mean(((blue45_boost_1f1 + blue45_boost_1f2) / 2)[ind_occ])
 # ----------------------------------------------------------------------------
 
 # +++ TEST +++
@@ -366,7 +382,7 @@ im, _ = mne.viz.plot_topomap(snrs_cnd4_1f2_avg, epochs.info, axes=axes[1, 1],
 axes[1, 1].set(title='CND4 GR(G) 1f2')
 cp.add_snr_colorbar(fig, axes[1, 1], im)
 
-# @@@ green45 boost pairs
+# @@@ blue45 boost pairs
 # Topography map of CND4, GR(G), 1f1:
 im, _ = mne.viz.plot_topomap(snrs_cnd4_1f1_avg, epochs.info, axes=axes[2, 0],
                              vlim=(vmin, vmax), show=False)
@@ -403,15 +419,15 @@ im, _ = mne.viz.plot_topomap(red135_boost_1f2, epochs.info, axes=axes[1, 2],
 axes[1, 2].set(title='red135 boost 1f2')
 cp.add_snr_colorbar(fig, axes[1, 2], im)
 
-# Topography map of green45 boost at 1f1:
-im, _ = mne.viz.plot_topomap(green45_boost_1f1, epochs.info, axes=axes[2, 2],
+# Topography map of blue45 boost at 1f1:
+im, _ = mne.viz.plot_topomap(blue45_boost_1f1, epochs.info, axes=axes[2, 2],
                              vlim=(vmin, vmax), show=False)
-axes[2, 2].set(title='green45 boost 1f1')
+axes[2, 2].set(title='blue45 boost 1f1')
 cp.add_snr_colorbar(fig, axes[2, 2], im)
-# Topography map of green45 boost at 1f2:
-im, _ = mne.viz.plot_topomap(green45_boost_1f2, epochs.info, axes=axes[3, 2],
+# Topography map of blue45 boost at 1f2:
+im, _ = mne.viz.plot_topomap(blue45_boost_1f2, epochs.info, axes=axes[3, 2],
                              vlim=(vmin, vmax), show=False)
-axes[3, 2].set(title='green45 boost 1f2')
+axes[3, 2].set(title='blue45 boost 1f2')
 cp.add_snr_colorbar(fig, axes[3, 2], im)
 
 # save figure
@@ -423,15 +439,15 @@ plt.savefig(os.path.join(save_path, f'{sub_id}_'
 
 fig, axs = plt.subplots(1, 2, figsize=(5, 4), sharey=True)
 fig.suptitle(f'Subject ID: {sub_id} – Avg. SNR improvement')
-axs[0].bar([1, 2], [red135_boost, green45_boost], color='grey')
+axs[0].bar([1, 2], [red135_boost, blue45_boost], color='grey')
 axs[0].set(title='All channels', xticks=[1, 2],
-           xticklabels=['red135', 'green45'], ylabel='SNR improvement')
+           xticklabels=['red135', 'blue45'], ylabel='SNR improvement')
 cp.trim_axes(axs[0])
 cp.prep4ai()
 
-axs[1].bar([1, 2], [red135_boost_occ, green45_boost_occ], color='grey')
+axs[1].bar([1, 2], [red135_boost_occ, blue45_boost_occ], color='grey')
 axs[1].set(title='Occipital channels', xticks=[1, 2],
-           xticklabels=['red135', 'green45'])
+           xticklabels=['red135', 'blue45'])
 cp.trim_axes(axs[1])
 cp.prep4ai()
 
@@ -442,37 +458,40 @@ plt.savefig(os.path.join(save_path, f'{sub_id}_'
                                     f'{rec_date}_avg_SNR_improvement.pdf'))
 
 # ----------------------------------------------------------------------------
-
-# @@@ PLOT BOOST ACROSS TRIALS @@@
-ind_occ = [65, 68, 69, 70, 72, 73, 74, 75, 80, 81, 82, 83, 87, 88]
-
-ch_avg_cnd1 = snrs_cnd1_1f1[:, ind_occ].mean(axis=1)
-ch_avg_cnd2 = snrs_cnd2_1f1[:, ind_occ].mean(axis=1)
-ch_avg_cnd3 = snrs_cnd3_1f1[:, ind_occ].mean(axis=1)
-ch_avg_cnd4 = snrs_cnd4_1f1[:, ind_occ].mean(axis=1)
-ch_avg_cnd1_blocked_1f1 = np.mean(np.reshape(ch_avg_cnd1, (4, 8)), axis=1)
-ch_avg_cnd2_blocked_1f1 = np.mean(np.reshape(ch_avg_cnd2, (4, 8)), axis=1)
-ch_avg_cnd3_blocked_1f1 = np.mean(np.reshape(ch_avg_cnd3, (4, 8)), axis=1)
-ch_avg_cnd4_blocked_1f1 = np.mean(np.reshape(ch_avg_cnd4, (4, 8)), axis=1)
-
-ch_avg_cnd2 = snrs_cnd2_1f2[:, ind_occ].mean(axis=1)
-ch_avg_cnd3 = snrs_cnd3_1f2[:, ind_occ].mean(axis=1)
-ch_avg_cnd1 = snrs_cnd1_1f2[:, ind_occ].mean(axis=1)
-ch_avg_cnd4 = snrs_cnd4_1f2[:, ind_occ].mean(axis=1)
-ch_avg_cnd1_blocked_1f2 = np.mean(np.reshape(ch_avg_cnd1, (4, 8)), axis=1)
-ch_avg_cnd2_blocked_1f2 = np.mean(np.reshape(ch_avg_cnd2, (4, 8)), axis=1)
-ch_avg_cnd3_blocked_1f2 = np.mean(np.reshape(ch_avg_cnd3, (4, 8)), axis=1)
-ch_avg_cnd4_blocked_1f2 = np.mean(np.reshape(ch_avg_cnd4, (4, 8)), axis=1)
-
-red_boost_1f1_blocked = ch_avg_cnd1_blocked_1f1 - ch_avg_cnd3_blocked_1f1
-red_boost_1f2_blocked = ch_avg_cnd2_blocked_1f2 - ch_avg_cnd4_blocked_1f2
-
-green_boost_1f1_blocked = ch_avg_cnd4_blocked_1f1 - ch_avg_cnd2_blocked_1f1
-green_boost_1f2_blocked = ch_avg_cnd3_blocked_1f2 - ch_avg_cnd1_blocked_1f2
-
-red_boost_occ_blocked = (red_boost_1f1_blocked + red_boost_1f2_blocked)/2
-green_boost_occ_blocked = (green_boost_1f1_blocked + green_boost_1f2_blocked)/2
-
-fig, ax = plt.subplots()
-ax.plot(red_boost_occ_blocked)
-ax.plot(green_boost_occ_blocked)
+#
+# # @@@ PLOT BOOST ACROSS TRIALS @@@
+# ind_occ = [65, 68, 69, 70, 72, 73, 74, 75, 80, 81, 82, 83, 87, 88]
+# # the aim is to average over 8 trials
+# # how many 'blocks' of 8 trials will there be?
+# kk = int(n_trials / 4 / 8)
+#
+# ch_avg_cnd1 = snrs_cnd1_1f1[:, ind_occ].mean(axis=1)
+# ch_avg_cnd2 = snrs_cnd2_1f1[:, ind_occ].mean(axis=1)
+# ch_avg_cnd3 = snrs_cnd3_1f1[:, ind_occ].mean(axis=1)
+# ch_avg_cnd4 = snrs_cnd4_1f1[:, ind_occ].mean(axis=1)
+# ch_avg_cnd1_blocked_1f1 = np.mean(np.reshape(ch_avg_cnd1, (kk, 8)), axis=1)
+# ch_avg_cnd2_blocked_1f1 = np.mean(np.reshape(ch_avg_cnd2, (kk, 8)), axis=1)
+# ch_avg_cnd3_blocked_1f1 = np.mean(np.reshape(ch_avg_cnd3, (kk, 8)), axis=1)
+# ch_avg_cnd4_blocked_1f1 = np.mean(np.reshape(ch_avg_cnd4, (kk, 8)), axis=1)
+#
+# ch_avg_cnd2 = snrs_cnd2_1f2[:, ind_occ].mean(axis=1)
+# ch_avg_cnd3 = snrs_cnd3_1f2[:, ind_occ].mean(axis=1)
+# ch_avg_cnd1 = snrs_cnd1_1f2[:, ind_occ].mean(axis=1)
+# ch_avg_cnd4 = snrs_cnd4_1f2[:, ind_occ].mean(axis=1)
+# ch_avg_cnd1_blocked_1f2 = np.mean(np.reshape(ch_avg_cnd1, (kk, 8)), axis=1)
+# ch_avg_cnd2_blocked_1f2 = np.mean(np.reshape(ch_avg_cnd2, (kk, 8)), axis=1)
+# ch_avg_cnd3_blocked_1f2 = np.mean(np.reshape(ch_avg_cnd3, (kk, 8)), axis=1)
+# ch_avg_cnd4_blocked_1f2 = np.mean(np.reshape(ch_avg_cnd4, (kk, 8)), axis=1)
+#
+# red_boost_1f1_blocked = ch_avg_cnd1_blocked_1f1 - ch_avg_cnd3_blocked_1f1
+# red_boost_1f2_blocked = ch_avg_cnd2_blocked_1f2 - ch_avg_cnd4_blocked_1f2
+#
+# blue_boost_1f1_blocked = ch_avg_cnd4_blocked_1f1 - ch_avg_cnd2_blocked_1f1
+# blue_boost_1f2_blocked = ch_avg_cnd3_blocked_1f2 - ch_avg_cnd1_blocked_1f2
+#
+# red_boost_occ_blocked = (red_boost_1f1_blocked + red_boost_1f2_blocked)/2
+# blue_boost_occ_blocked = (blue_boost_1f1_blocked + blue_boost_1f2_blocked)/2
+#
+# fig, ax = plt.subplots()
+# ax.plot(red_boost_occ_blocked)
+# ax.plot(blue_boost_occ_blocked)
